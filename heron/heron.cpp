6bdd238ced495e0a38d25ba578ff6125c7d340f2
Julian Arz <j.arz@web.de>
2021-07-25 23:30:28 +0200

    cleans up and prepares talk
    * removes wasmtime-py example because it was broken.
    * uses int sqrt example because wasmtime cli cannot cope with floats
    * inside source build for heron makes talk easier

--- heron/heron.cpp
+++ heron/heron.cpp
@@ -1,13 +1,9 @@
 extern "C" {
-    double heron(const double number) {
+    double heron(const int number) {
         double x = 42.0;
         for (int i = 0; i < 1000; ++i) {
             x = (x + number / x) / 2.0;
         }
         return x;
-    }
-
-    double heron_int(const int number) {
-        return heron(number);
     }
 }
--- heron/run_heron.py
+++
@@ -1,4 +0,0 @@
-import wasmtime.loader
-import heron
-
-print("sqrt(2) = " + str(heron.heron_int(2)))
