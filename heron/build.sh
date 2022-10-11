5d0c599423b61861ed64d0dfd894d1d02bb87c12
Julian Arz <j.arz@web.de>
2022-10-11 16:03:08 +0200

    fixes function name in heron example

--- heron/build.sh
+++ heron/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -f heron.wasm
 rm -f heron.wat
-emcc heron.cpp --no-entry -Os -s EXPORTED_FUNCTIONS=[_heron,_heron_int] -o heron.wasm
+emcc heron.cpp --no-entry -Os -s EXPORTED_FUNCTIONS=[_heron] -o heron.wasm
 ~/wabt/build/wasm2wat heron.wasm -o heron.wat
